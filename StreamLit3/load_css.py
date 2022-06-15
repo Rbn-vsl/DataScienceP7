f568aff14ad1ac1601e1b8318760aafd1733d2f6
Rbn-vsl <42212880+Rbn-vsl@users.noreply.github.com>
2022-06-15 12:11:43 +0200

    Update load_css.py

--- StreamLit3/load_css.py
+++ StreamLit3/load_css.py
@@ -4,11 +4,10 @@ https://discuss.streamlit.io/t/are-you-using-html-in-markdown-tell-us-why/96/25
 import streamlit as st
 import os
 
-# Get the current working directory
-cwd = os.getcwd()
-name = "style.css"
-file_name = os.path.join(cwd, name)
-
 def local_css(file_name):
+    # Get the current working directory
+    cwd = os.getcwd()
+    name = "style.css"
+    file_name = os.path.join(cwd, name)
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
