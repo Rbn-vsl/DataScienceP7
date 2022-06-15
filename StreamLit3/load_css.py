6f485a47cb9e470a48401677d31f835b5e516c48
Rbn-vsl <42212880+Rbn-vsl@users.noreply.github.com>
2022-06-15 12:16:33 +0200

    Update load_css.py

--- StreamLit3/load_css.py
+++ StreamLit3/load_css.py
@@ -7,7 +7,9 @@ import os
 def local_css(file_name):
     # Get the current working directory
     cwd = os.getcwd()
+    root = "StreamLit3"
+    file_path = os.path.join(cwd, root)
     name = "style.css"
-    file_name = os.path.join(cwd, name)
+    file_name = os.path.join(file_path, name)
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
